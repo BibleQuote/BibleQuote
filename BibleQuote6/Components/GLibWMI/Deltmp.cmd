d2262845cbcbca34dd188bd6ad99dfa7d0f1f9d8
Ruslan Gilmutdinov <gilmutdinov.ruslan@gmail.com>
2018-01-31 00:10:50 +0600

    #5: Project structure reorganization (wip)
    - Moved third-party libraries to .\Components subfolder;
    - Moved DLL libraries (7za.dll and BQsqlite3.dll) to .\Libs folder;
    - Fixed references
    - Removed unused files (SandBox folder)

---
+++ BibleQuote6/Components/GLibWMI/Deltmp.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+@echo ===============================================
+@echo ====      Borrando ficheros temporales     ====
+@echo ===============================================
+@echo == 	Archivos *.~*
+@echo on
+del *.~* /s /Q	
+@echo == 	Archivos *.bak
+del *.bak /s /Q	
+@echo == 	Archivos *.old
+del *.old /s /Q	
+@echo == 	Ficheros *.ddp
+del *.ddp /s /Q	
+@echo == 	Ficheros *.map
+del *.map /s /Q	
+echo ---------------------------------------------
+@echo on
